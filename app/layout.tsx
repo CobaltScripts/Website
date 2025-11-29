4bed98dc6d762301af9ee56e0b427001a03dc113
Nathan <209938737+quiteboring@users.noreply.github.com>
2025-11-29 16:42:44 -0500

    feat: update metadata for discord embed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
@@ -18,8 +18,30 @@ const poppinsMono = Poppins({
 export const metadata: Metadata = {
   title: "Cobalt",
   description: "Experience the next level of Skyblock and maximize your grinding potential with Cobalt.",
+
+  openGraph: {
+    title: 'Cobalt',
+    description: 'Experience the next level of Skyblock and maximize your grinding potential with Cobalt.',
+    url: 'https://cobalt.quiteboring.dev',
+    siteName: 'cobalt.quiteboring.dev',
+
+    images: [
+      {
+        url: 'https://cobalt.quiteboring.dev/logo.png',
+        width: 250,
+        height: 250,
+      },
+    ],
+
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
+export const viewport: Viewport = {
+  themeColor: '#4682B4',
+}
+ 
 export default function RootLayout({
   children,
 }: Readonly<{
